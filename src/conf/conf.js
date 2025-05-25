0fc49bd135b6defc2ff8381623c119dde7dbe325
Viraj-Kaushik <kaushikviraj5@gmail.com>
2025-05-25 15:58:07 +0530

    CORS issue resolved

--- src/conf/conf.js
+++ src/conf/conf.js
@@ -10,15 +10,6 @@ const conf = {
 }
 
 
-console.log('Debug env vars:', {
-    appwriteUrl: import.meta.env.VITE_APPWRITE_URL,
-    projectId: import.meta.env.VITE_APPWRITE_PROJECT_ID,
-    databaseId: import.meta.env.VITE_APPWRITE_DATABASE_ID,
-    collectionId: import.meta.env.VITE_APPWRITE_COLLECTION_ID,
-    bucketId: import.meta.env.VITE_APPWRITE_BUCKET_ID,
-    tinymceID: import.meta.env.VITE_TINYMCE_ID,
-})
-
 export default conf
 
 
