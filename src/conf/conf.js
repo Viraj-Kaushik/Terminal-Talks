86815ae63ca4e3ade8db36986dedcd30f2376b6c
Viraj-Kaushik <kaushikviraj5@gmail.com>
2025-05-25 15:46:34 +0530

    env errors in deployment

--- src/conf/conf.js
+++ src/conf/conf.js
@@ -1,6 +1,6 @@
 const conf = {
 
-    appwriteUrl : String(import.meta.env.VITE_APPWRITE_URL),
+    appwriteUrl : String(import.meta.env.VITE_APPWRITE_URL || ''),
     appwriteProjectId : String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
     appwriteDatabaseId : String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
     appwriteCollectionId : String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
@@ -9,4 +9,17 @@ const conf = {
 
 }
 
+
+console.log('Debug env vars:', {
+    appwriteUrl: import.meta.env.VITE_APPWRITE_URL,
+    projectId: import.meta.env.VITE_APPWRITE_PROJECT_ID,
+    databaseId: import.meta.env.VITE_APPWRITE_DATABASE_ID,
+    collectionId: import.meta.env.VITE_APPWRITE_COLLECTION_ID,
+    bucketId: import.meta.env.VITE_APPWRITE_BUCKET_ID,
+    tinymceID: import.meta.env.VITE_TINYMCE_ID,
+})
+
 export default conf
+
+
+
